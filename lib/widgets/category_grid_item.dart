61831bfdb6d26f5a9a754cfe765f405137738657
weali05 <alfredwemali05@gmail.com>
2024-12-21 21:22:30 +0300

    widgets and screens

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget{
+  const CategoryGridItem({super.key, this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+  
+  }
+}
