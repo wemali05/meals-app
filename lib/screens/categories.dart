666dc7e795155cb33483d549208541b331632b9d
weali05 <alfredwemali05@gmail.com>
2024-12-21 21:56:06 +0300

    display categories on screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +13,16 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your Category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white),),
-          Text('2', style: TextStyle(color: Colors.white),),
-          Text('3', style: TextStyle(color: Colors.white),),
-          Text('4', style: TextStyle(color: Colors.white),),
-          Text('5', style: TextStyle(color: Colors.white),),
-          Text('6', style: TextStyle(color: Colors.white),),
+        children: [
+          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          for(final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,13 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget{
-  const CategoryGridItem({super.key, this.category});
+  const CategoryGridItem({super.key, required this.category});
 
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-  
+     return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          category.color.withOpacity(0.55),
+          category.color.withOpacity(0.9),
+        ],),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onBackground
+        ),
+      ),
+     );
   }
 }
