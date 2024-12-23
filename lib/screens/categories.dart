ce34a3cdba3479367acb4069a16d2eb6fa9f5915
weali05 <alfredwemali05@gmail.com>
2024-12-23 15:53:35 +0300

    passing data to target sreens

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectedCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals),
+      ),
+    ); // Navigator.push(context, Route)
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +36,13 @@ class CategoriesScreen extends StatelessWidget {
             mainAxisSpacing: 20),
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-          for(final category in availableCategories)
-            CategoryGridItem(category: category)
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectGategory: () {
+                _selectedCategory(context, category);
+              },
+            )
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem({super.key, required this.category, required this.onSelectGategory});
 
   final Category category;
+  final void Function() onSelectGategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        onSelectGategory();
+      },
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
