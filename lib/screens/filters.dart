39e3a8f1c2204a26f157275186d38343788f60fe
weali05 <alfredwemali05@gmail.com>
2024-12-25 13:36:04 +0300

    replace deprecated willpop with with popscope

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -41,16 +41,16 @@ class _FilterScreenState extends State<FiltersScreen> {
       //     }
       //   },
       // ),
-      body: WillPopScope(
-        onWillPop: () async {
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool dipPop, dynamic result) {
+          if(dipPop) return;
           Navigator.of(context).pop({
             Filter.glutenFree: _glutenFreeFilterSet,
             Filter.lactoseFree: _lactoseFreeFilterSet,
             Filter.vegetarian: _vegetarianFilterSet,
             Filter.vegan: _veganFilterSet
           });
-
-          return false;
         },
         child: Column(
           children: [
